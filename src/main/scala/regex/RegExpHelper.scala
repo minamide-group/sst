f7b70ffa48372283011907d98e901ede0d07de16
leaf67697 <leaf67697@gmail.com>
2018-12-07 14:33:28 +0900

    Update RegExpHelper.scala

--- src/main/scala/regex/RegExpHelper.scala
+++ src/main/scala/regex/RegExpHelper.scala
@@ -13,11 +13,16 @@ trait RegExpHelper[T, Γ] {
       finialStates.map(qf => {
         val rules = eliminate(states.filterNot(x => x == q0).filterNot(x => x == qf).toList,
           delta.map(x => (x._1, CharExp(h(x._4)): RegExp, x._3)))
-        val A = getCombined(q0, q0, rules)
-        val B = getCombined(q0, qf, rules)
-        val C = getCombined(qf, q0, rules)
-        val D = getCombined(qf, qf, rules)
-        ConcatExp(ConcatExp(StarExp(AltExp(A, ConcatExp(ConcatExp(B, StarExp(D)), C))), B), StarExp(D))
+
+        if(q0==qf)
+          getCombined(q0, q0, rules)
+        else {
+          val A = getCombined(q0, q0, rules)
+          val B = getCombined(q0, qf, rules)
+          val C = getCombined(qf, q0, rules)
+          val D = getCombined(qf, qf, rules)
+          ConcatExp(ConcatExp(StarExp(AltExp(A, ConcatExp(ConcatExp(B, StarExp(D)), C))), B), StarExp(D))
+        }
       })
     }).foldLeft(EmptyExp: RegExp) { (x, y) => AltExp(x, y) }
   }
@@ -28,8 +33,8 @@ trait RegExpHelper[T, Γ] {
   private def eliminate[Q](states: List[Q], rules: Set[(Q, RegExp, Q)]): Set[(Q, RegExp, Q)] = {
     states match {
       case s :: rest =>
-        val newRules: Set[(Q, RegExp, Q)] = rules.filter(x => x._3 == s).flatMap(r1 => {
-          rules.filter(x => x._1 == s).map(r2 => (r1._1, AltExp(getCombined(r1._1, r2._3, rules), ConcatExp(ConcatExp(r1._2, StarExp(getCombined(s, s, rules))), r2._2)), r2._3))
+        val newRules: Set[(Q, RegExp, Q)] = rules.filterNot(x => x._1==s && x._3==s).filter(x => x._3 == s).flatMap(r1 => {
+          rules.filterNot(x => x._1==s && x._3==s).filter(x => x._1 == s).map(r2 => (r1._1, AltExp(getCombined(r1._1, r2._3, rules), ConcatExp(ConcatExp(r1._2, StarExp(getCombined(s, s, rules))), r2._2)), r2._3))
         })
         eliminate(rest, newRules.union(rules.filterNot(x => x._1 == s).filterNot(x => x._3 == s).filterNot(x => newRules.map(r => (r._1, r._3)).contains(x._1, x._3))))
       case Nil => rules
