757bf9e578ea3071d43ec4609c5b451fb154a71e
leaf67697 <leaf67697@gmail.com>
2018-12-07 14:33:20 +0900

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,14 @@
-
+import regex.MapRegExp._
 object Main extends App {
 
   val sst = SSTFactory.getTestSST()
   sst.toParikhImage.foreach(println)
+//  val re = AltExp(
+//    ConcatExp(ConcatExp(CharExp(Set((Map(0 -> 2, 1 -> 0),Set()))),StarExp(CharExp(Set((Map(0 -> 2, 1 -> 0),Set()))))),CharExp(Set((Map(0 -> 0, 1 -> 3),Set())))),
+//    ConcatExp(ConcatExp(ConcatExp(ConcatExp(CharExp(Set((Map(0 -> 2, 1 -> 0),Set()))),StarExp(CharExp(Set((Map(0 -> 2, 1 -> 0),Set()))))),CharExp(Set((Map(0 -> 0, 1 -> 3),Set())))),StarExp(CharExp(Set((Map(0 -> 0, 1 -> 3),Set()))))),CharExp(Set((Map(0 -> 0, 1 -> 3),Set()))))
+//  )
+//
+//  eval(re) match {
+//    case m : CharExp => m.c.foreach(println)
+//  }
 }
